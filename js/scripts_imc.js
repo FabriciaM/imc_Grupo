cfb3b57045799f50aea3626715528455a75b8c24
FabriciaM <fabriciamanoelle@icloud.com>
2026-04-24 08:56:16 -0300

    Capturando o evento

--- js/scripts_imc.js
+++ js/scripts_imc.js
@@ -1,3 +1,19 @@
 const formImc = document.querySelector('.formulario');
 const btnCalcular = document.querySelector('#btn-calcular');
 const divLista = document.querySelector('#resultado');
+
+const pessoas = [];
+
+btnCalcular.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const formPessoa = new FormData(formImc);
+
+    const objPessoa = {
+        nome: formPessoa.get('nome'),
+        sexo: formPessoa.get('sexo'),
+        dataNascimento: formPessoa.get('dataNascimento'),
+        peso: parseFloat(formPessoa.get('peso')),
+        altura: parseFloat(formPessoa.get('altura'))
+    }
+});
